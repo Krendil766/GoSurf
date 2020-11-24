1c368fa5a0c6c7447eefd4c2cc95ba01075809c3
Krendil766 <krendil766@gmail.com>
2020-11-24 22:51:31 +0300

    First page and animation

--- app/js/main.js
+++ app/js/main.js
@@ -3,11 +3,11 @@ $(function() {
     $('.header__slider').slick({
         infinite: true,
         fade: true,
-        prevArrow:'<img class = "slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
-        nextArrow:'<img class = "slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-        asNavFor: '.slick-dots',
+        prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slider-dots',
     });
-    $('.slick-dots').slick({
+    $('.slider-dots').slick({
         slidesToShow: 4,
         slidesToScroll: 4,
         asNavFor: '.header__slider',
