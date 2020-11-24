0b85eaaf4559743b12e9ec506ad43998f4a0a019
Krendil766 <krendil766@gmail.com>
2020-11-24 19:15:18 +0300

    added header slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,17 @@
 $(function() {
 
     $('.header__slider').slick({
-        infiniti: true
-    })
+        infinite: true,
+        fade: true,
+        prevArrow:'<img class = "slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow:'<img class = "slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+        asNavFor: '.slick-dots',
+    });
+    $('.slick-dots').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        asNavFor: '.header__slider',
+    });
 
 
 });
