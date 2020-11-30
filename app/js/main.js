2bdf141a19177d8a2e5d5dbd84449f5c584cae0b
Krendil766 <krendilek766@gmail.com>
2020-11-30 16:25:50 +0300

    Finish third page

--- app/js/main.js
+++ app/js/main.js
@@ -25,6 +25,12 @@ $(function() {
         arrows: false,
         asNavFor: '.serf-slider',
         focusOnSelect: true,
+    });
+    $('.travel__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
     })
 
 
