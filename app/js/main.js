8448166c3a13901b8898efacb45a6026ef6a61b1
Krendil766 <krendilek766@gmail.com>
2020-12-01 17:03:34 +0300

    Added finish page

--- app/js/main.js
+++ app/js/main.js
@@ -1,70 +1,74 @@
-$(function() {
+$(function () {
 
-    $('.header__slider').slick({
-        infinite: true,
-        fade: true,
-        prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
-        nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-        asNavFor: '.slider-dotshead',
-    });
-    $('.slider-dotshead').slick({
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        asNavFor: '.header__slider',
-    });
-    $('.serf-slider').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
-        nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-        asNavFor: '.slider-map',
-    });
-    $('.slider-map').slick({
-        slidesToShow: 8,
-        slidesToScroll: 1,
-        arrows: false,
-        asNavFor: '.serf-slider',
-        focusOnSelect: true,
-    });
-    $('.holder__slider').slick({
-        infinite: true,
-        fade: true,
-        prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
-        nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-    });
+  $('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+    nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+    asNavFor: '.slider-dotshead',
+  });
+  $('.slider-dotshead').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+  });
+  $('.serf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+    nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+    asNavFor: '.slider-map',
+  });
+  $('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.serf-slider',
+    focusOnSelect: true,
+  });
+  $('.holder__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class = "slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+    nextArrow: '<img class = "slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+  });
 
-    $('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
-    $('.quantity').each(function() {
-      var spinner = $(this),
-        input = spinner.find('input[type="number"]'),
-        btnUp = spinner.find('.quantity-up'),
-        btnDown = spinner.find('.quantity-down'),
-        min = input.attr('min'),
-        max = input.attr('max');
+  $('<div class="quantity-nav"><div class="quantity-button quantity-down">-</div><div class="quantity-button quantity-up">+</div></div>').insertAfter('.quantity input');
+  $('.quantity').each(function () {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.quantity-up'),
+      btnDown = spinner.find('.quantity-down'),
+      min = input.attr('min'),
+      max = input.attr('max');
 
-      btnUp.click(function() {
-        var oldValue = parseFloat(input.val());
-        if (oldValue >= max) {
-          var newVal = oldValue;
-        } else {
-          var newVal = oldValue + 1;
-        }
-        spinner.find("input").val(newVal);
-        spinner.find("input").trigger("change");
-      });
-
-      btnDown.click(function() {
-        var oldValue = parseFloat(input.val());
-        if (oldValue <= min) {
-          var newVal = oldValue;
-        } else {
-          var newVal = oldValue - 1;
-        }
-        spinner.find("input").val(newVal);
-        spinner.find("input").trigger("change");
-      });
-
+    btnUp.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
     });
 
+    btnDown.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find("input").val(newVal);
+      spinner.find("input").trigger("change");
+    });
 
+  });
+  $('.quantity-button').on('click', function () {
+    let summ = ($('.guests').val() - 1) * $('.summ').data('guests') + $('.nights').val() * $('.summ').data('nights')
+    $('.summ').html("$" + summ);
+  });
+  let summ = ($('.guests').val() - 1) * $('.summ').data('guests') + $('.nights').val() * $('.summ').data('nights')
+    $('.summ').html("$" + summ );
 });
